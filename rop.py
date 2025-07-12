c90e52d7bf3e8b01b1ab0fa0bdb941dc221bec26
Shawn Selbst <shselbst@gmail.com>
2025-07-12 19:29:46 -0400

    Get rop gadgets from file in python

---
+++ rop.py
@@ -0,0 +1,20 @@
+from ropper import RopperService
+from typing import Optional
+import os
+
+
+def get_gadgets(fname: str, all_gadgets: Optional[bool] = False, inst_count: Optional[int] = 6, arch: Optional[str] = 'x86_64') -> str:
+    options = {'color' : False,
+               'all': all_gadgets,
+               'inst_count': inst_count}
+
+    rs = RopperService(options)
+
+    rs.addFile(fname)
+    rs.setArchitectureFor(name=fname, arch=arch)
+    rs.loadGadgetsFor(name=fname)
+    gadgets:str  = rs.getFileFor(name=fname).gadgets
+    rs.removeFile(fname)
+    return gadgets
+def get_num_gadgets(fname: str, all_gadgets: Optional[bool] = False, inst_count: Optional[int] = 6, arch: Optional[str] = 'x86_64') -> int:
+    return len(get_gadgets(fname, all_gadgets, inst_count, arch))
